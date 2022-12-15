02ac03bbb2c2c99d70b0e267b5ad4e74d4e49bbf
Tensai99 <eimtingwi@gmail.com>
2022-12-14 16:45:28 -0800

    101 task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - computes and prints the sum of all the multpiles
+ *  of 3 or 5 below 1024
+ *  Return: 0
+ */
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 0;
+	b = 0;
+	c = 0;
+
+	for (i = 0; 1 < 1024; ++1)
+	{
+		if ((i % 3) == 0)
+		{
+			a = a + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			b = b + i;
+		}
+	}
+	c = a + b;
+	printf("%lu\n", c);
+	return (0);
+}
+
