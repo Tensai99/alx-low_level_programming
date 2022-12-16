487fc74f40930c2d8f02ac844864abbd690f8e19
Tensai99 <eimtingwi@gmail.com>
2022-12-16 13:57:38 -0800

    + / -

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negativ(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void pritn_remaing_days(int month, int day, int year);
+
+#endif
