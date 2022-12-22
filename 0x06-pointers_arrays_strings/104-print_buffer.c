2f04fca428e82a65c433bd302325484ec213138b
Tensai99 <eimtingwi@gmail.com>
2022-12-22 01:13:54 -0800

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -45,9 +45,13 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", i * 10);
 
 		if (i < size / 10)
+		{
 			print_line(b, 9, i);
+		}
 		else
+		{
 			print_line(b, size % 10 - 1, i);
+		}
 		putchar('\n');
 	}
 	if (size == 0)
