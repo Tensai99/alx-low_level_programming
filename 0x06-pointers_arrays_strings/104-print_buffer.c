8058e9d1c1e1c2c2b0464c141d8f509517340479
Tensai99 <eimtingwi@gmail.com>
2022-12-22 04:00:52 -0800

    fixed

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,51 +1,109 @@
 #include "main.h"
-# include <stdio.h>
+
+#include <stdio.h>
 
 /**
- * print_buffer - prints buffer
- * @b: the buffer to be printed
- * @size: number of bytes to be printed
+ * print_line - prints a s bytes of a buffer
+ *
+ * @c: buffer to print
+ *
+ * @s: bytes of buffer to print
+ *
+ * @l: line of buffer to print
+ *
+ * Return: void
+ *
  */
-void print_buffer(char *b, int size)
+
+
+
+void print_line(char *c, int s, int l)
+
 {
-	int byte, index;
 
-	for (byte = 0; byte < size; byte += 10)
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+
 	{
-		printf("%08x: ", byte);
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf(" ");
+		if (j <= s)
 
-			else
-				printf("%02x", *(b + index + byte));
+			printf("%02x", c[l * 10 + j]);
 
+		else
 
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
+			printf("  ");
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
+		if (j % 2)
 
-			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
+			putchar(' ');
 
-			else
-				printf(".");
-		}
-
-		if (byte >= size)
-			continue;
-
-		printf("\n");
 	}
 
-	if (size <= 0)
-		printf("\n");
+	for (k = 0; k <= s; k++)
+
+	{
+
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+
+			putchar(c[l * 10 + k]);
+
+		else
+
+			putchar('.');
+
+	}
+
+}
+
+
+
+/**
+ * print_buffer - prints a buffer
+ *
+ * @b: buffer to print
+ *
+ * @size: size of buffer
+ *
+ * Return: void
+ *
+ */
+
+void print_buffer(char *b, int size)
+
+{
+
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+
+	{
+
+		printf("%08x: ", i * 10);
+
+		if (i < size / 10)
+
+		{
+
+			print_line(b, 9, i);
+
+		}
+
+		else
+
+		{
+
+			print_line(b, size % 10 - 1, i);
+
+		}
+
+		putchar('\n');
+
+	}
+
+	if (size == 0)
+
+		putchar('\n');
+
 }
